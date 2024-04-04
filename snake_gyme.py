cf36494fbec16edb79f3e74628e14cbb711dc8f1
Gabriela Madalska <gabriela.madalska@gmail.com>
2024-04-04 13:18:58 +0200

    Wyświetlenie snake

--- snake_gyme.py
+++ snake_gyme.py
@@ -5,9 +5,28 @@ pygame.init()
 
 # stałe
 WIDTH, HEIGHT = 800, 600
+CELL_SIZE = 20
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+
+class Snake:
+    def __init__(self):
+        self.body = [(WIDTH // 2, HEIGHT // 2)]
+
+    def draw(self, screen):
+        for segment in self.body:
+            pygame.draw.rect(screen, WHITE, (segment[0], segment[1], CELL_SIZE, CELL_SIZE))   
+    
+    
+
 
 def game():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    # ustawienie okna
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), )
+    screen.fill(BLACK)
+    pygame.display.update()
     pygame.display.set_caption("Snake")
 
     # głowna petla gry
@@ -18,6 +37,12 @@ def game():
                 running = False
 
             #dodanie poruszania się elif
+
+        snake = Snake()
+        pygame.display.flip()
+        snake.draw(screen)
+        #food.draw(screen)
+        #clock.tick(FPS)
 
 
     pygame.quit()
